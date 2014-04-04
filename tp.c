03fc5a82cd3d22786b7c9734c71c68b98bf3070b
luiguiMV <luiguimv198@hotmail.com>
2014-04-03 23:59:44 -0600

    ultimo commit

--- tp.c
+++ tp.c
@@ -17,7 +17,7 @@ int ultimoCont;//variable tipo int que guarda el numero de la posicion del ultim
 /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
 
-//Funcion main basicamente llama a la funcion menu y llena el struct
+//Funcion main basicamente llama a la funcion menu y llena el struct con la info del txt
 void main(){
 	leerTxt();//llena el struct con los datos del txt
 	menu();
@@ -26,20 +26,28 @@ void main(){
 /*Funcion muestra el menu al inicio*/
 int menu(){
 	int opc;
-	printf("\n\nEscriba el número de la acción que desea ejecutar y presione Enter...\n1.Agregar Contactos\n2.Enviar Mensaje\n3.Ver lista de contactos\n4.Salir\n");
+	printf("\n\nEscriba el número de la acción que desea ejecutar y presione Enter...\n1.Agregar Contactos\n2.Enviar Mensaje\n3.Recibir mensaje\n4.Ver lista de contactos\n5.Salir\n");
 	scanf("%d", &opc);
 	if (opc==1){
 		datosContacto();
 	}
-	else if(opc==3){
+	else if(opc==4){
 		imprimeContactos();
+	}
+	else if(opc==3){
+		fork_();
 	}
 	else if(opc==2){
 		enviarMensaje();
 	}
+	else if(opc=5){
+		printf("Gracias, adios");
+		return 0;
+	}
 	return 0;
 }
 
+//enviarMensaje muestra en pantalla los contactos y solicita elgir uno
 int enviarMensaje(){
 	char ip[16];
 	char *ptr;
@@ -47,72 +55,71 @@ int enviarMensaje(){
 	printf("\nEnviar mensaje a:\n");
 	int opc = 0;
 	imprimeContactos_aux();
-	printf("Escriba el numero del cliente y presione Enter...\n");
-	scanf("%d", &opc);
-	//printf(listaContactos[opc-1].ip);
-	servidor(listaContactos[opc-1].puerto);
-	//cliente(li)
+	fork_();
 	
 }
 
 
-
-int ff(int argc, char *argv[]){
+///////////////////////////////////////////////////////////////////////
+//fork
+int fork_(int argc, char *argv[]){
 int estado;
 if(fork()){
-wait(estado);
-cliente();
+	wait(estado);
+	cliente();
 }
 
 else{
-
-servidor(20000);}
-
-
-
+	
+	
+	servidor();
+}
 }
 
-int servidor(int puerto)
+int servidor()
 {
+	
+int puerto;
+printf("Server, Introduzca el puerto: ");
+scanf("%d", &puerto);
 //Definir variables,estructuras
-struct sockaddr_in server;
-struct sockaddr_in client;
-int fd,fd2,longitud_cliente,numbytes;
+struct sockaddr_in servidor;
+struct sockaddr_in cliente;
+int sock,sock2,longitud_cliente,numbytes;
 char buf[1024]; //Para recibir mensaje
 char enviar2[1024]; //Para enviar mensaje
 char enviar[1024]; //Para enviar mensaje
 
 system("clear");
- printf("La direccion del servidor es 127.0.0.1\n\n");
  /*printf("Por favor introduzca el puerto de escucha: \n\n");*/
  /*scanf("%d",&puerto);*/
 
- server.sin_family= AF_INET;
- server.sin_port = htons(puerto);
- server.sin_addr.s_addr = INADDR_ANY;
- bzero(&(server.sin_zero),8);
+ servidor.sin_family= AF_INET;
+ servidor.sin_port = htons(puerto);
+ servidor.sin_addr.s_addr = INADDR_ANY;
+ bzero(&(servidor.sin_zero),8);
 
  //Definicion de socket
- if (( fd=socket(AF_INET,SOCK_STREAM,0) )<0){
+ if (( sock=socket(AF_INET,SOCK_STREAM,0) )<0){
  perror("Error de apertura de socket");
  exit(-1);
  }
 
  //Avisar al sistema que se creo un socket
- if(bind(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) {
+ if(bind(sock,(struct sockaddr*)&servidor, sizeof(struct sockaddr))==-1) {
  printf("error en bind() \n");
  exit(-1);
  }
 
  //Establecer el socket en modo escucha
- if(listen(fd,5) == -1) {
+ if(listen(sock,5) == -1) {
  printf("error en listen()\n");
  exit(-1);
  }
 
- printf("SERVIDOR EN ESPERA...\n");
+ printf("Esperando mensaje...\n");
  longitud_cliente= sizeof(struct sockaddr_in);
- if ((fd2 = accept(fd,(struct sockaddr *)&client,&longitud_cliente))==-1) {
+ if ((sock2 = accept(sock,(struct sockaddr *)&cliente,&longitud_cliente))==-1) {
  printf("error en accept()\n");
  exit(-1);
  }
@@ -120,12 +127,12 @@ system("clear");
  printf("------SESION INICIADA------\n");
  printf("CLIENTE CONECTADO\n");
  strcpy(enviar,"SERVIDOR CONECTADO...");
- send(fd2, enviar, 1024,0);
+ send(sock2, enviar, 1024,0);
 
 //Ciclo para enviar y recibir mensajes
  while(1){
  //El servidor espera el primer mensaje
- recv(fd2,buf,1024,0);
+ recv(sock2,buf,1024,0);
  if(strcmp(&buf,"salir")==0){
  break;
  }
@@ -134,56 +141,56 @@ system("clear");
  //El cliente recibe el mensaje del servidor
  printf("Escribir mensaje: ");
  scanf("%*c%[^\n]",enviar2);
- send(fd2,enviar2,1024,0);
+ send(sock2,enviar2,1024,0);
  if(strcmp(enviar2,"salir")==0){
  break;
  }
 }
- close(fd2);
- close(fd);
+ close(sock2);
+ close(sock);
 
 }
 
 
 
- cliente(int puerto1)
+ cliente()
 {
+ printf("soy cliente\n\n");
  //Definir variables,estructuras
- struct sockaddr_in server;
+ struct sockaddr_in servidor;
  char ip[16];
- int fd, numbytes,puerto;
+ int sock, numbytes,puerto;
  char buf[1024];
  char enviar[1024];
 
  system("clear");
 
-printf("ingrese la ip del servidor\n");
+ printf("Ingrese la ip del servidor\n");
  scanf("%s",ip);
 
-//printf("ingrese el puerto de conexion\n");
- //scanf("%d",&puerto);
- puerto = puerto1;
+ printf("Ingrese el puerto de conexion\n");
+ scanf("%d",&puerto);
 
  //Socket
- if ((fd=socket(AF_INET, SOCK_STREAM, 0))==-1){
+ if ((sock=socket(AF_INET, SOCK_STREAM, 0))==-1){
  printf("socket() error\n");
  exit(-1);
  }
 
  //Datos del servidor
- server.sin_family = AF_INET;
- server.sin_port = htons(puerto);
- server.sin_addr.s_addr=inet_addr(ip);
- bzero(&(server.sin_zero),8);
+ servidor.sin_family = AF_INET;
+ servidor.sin_port = htons(puerto);
+ servidor.sin_addr.s_addr=inet_addr(ip);
+ bzero(&(servidor.sin_zero),8);
 
 //Conectarse al servidor
- if(connect(fd, (struct sockaddr *)&server,
+ if(connect(sock, (struct sockaddr *)&servidor,
  sizeof(struct sockaddr))==-1){
  printf("connect() error\n");
  exit(-1);
  }
  //Recibir mensaje de bienvenida
- if ((numbytes=recv(fd,buf,1024,0)) == -1){
+ if ((numbytes=recv(sock,buf,1024,0)) == -1){
  printf("Error en recv() \n");
  exit(-1);
  }
@@ -193,21 +200,23 @@ while(1){
  //El servidor espera el primer mensaje
  printf("Escribir mensaje: ");
  scanf("%*c%[^\n]",enviar);
- send(fd,enviar,1024,0);
+ send(sock,enviar,1024,0);
  if(strcmp(enviar,"salir")==0){
  break;
  }
 
  //El cliente recibe el mensaje del servidor
- recv(fd,buf,1024,0);
+ recv(sock,buf,1024,0);
  if(strcmp(buf,"salir")==0){
  break;
  }
  printf("Servidor: %s\n",buf);
 }
- close(fd);
+ close(sock);
 }
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 /*Funcion que pregunta al usuario el nombre, ip y puerto, guarda esta info en un struct 
 */
 void datosContacto(){
